4690ffe8a8614e97b0882ca5eae0fc0583dbb14c
Artivis <deray.jeremie@gmail.com>
2014-05-05 09:20:27 +0200

    node to pub pano

--- src/Demo_Omni_Live.cpp
+++ src/Demo_Omni_Live.cpp
@@ -4,6 +4,12 @@
 
 #include <opencv2/core/core.hpp>
 
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv/cvwimage.h>
+#include <opencv/highgui.h>
+#include <cv_bridge/cv_bridge.h>
+
 #include <iostream>
 #include <string>
 
@@ -11,6 +17,7 @@
 int main(int argc, char** argv){
 
     ros::init(argc,argv, "omnicamera_live");
+    ros::NodeHandle nh;
 
     OmniCamera omniSys("");
 
@@ -20,20 +27,30 @@ int main(int argc, char** argv){
         return 2;
     }
 
+    const std::string panoTopic = "/omni_camera/panorama";
+
+    image_transport::ImageTransport imTrans(nh);
+    sensor_msgs::Image img_msg;
+    image_transport::Publisher pub = imTrans.advertise(panoTopic, 1);
+    cv_bridge::CvImage cv_img;
+
+    cv_img.encoding = "bgr8";
+
+
     omniSys.DispParam();
 
     omniSys.SetPanoSize(400,1000);
 
-    omniSys.MergeLUTWrap(true);
+    omniSys.MergeLUTWrap();
 
 
 
 //    // Test with images from disk
-//    double time;
-//    std::string imCam1 = "etc/images/right_frame0000.jpg";
-//    std::string imCam2 = "etc/images/left_frame0000.jpg";
-//    omniSys.camera_1->readImage(imCam1);
-//    omniSys.camera_2->readImage(imCam2);
+    double time;
+    std::string imCam1 = "etc/images/right_frame0000.jpg";
+    std::string imCam2 = "etc/images/left_frame0000.jpg";
+    omniSys.camera_1->readImage(imCam1);
+    omniSys.camera_2->readImage(imCam2);
 
 //    time = (double)cv::getTickCount();
 //    omniSys.StitchImage();
@@ -45,16 +62,26 @@ int main(int argc, char** argv){
 //    cv::destroyWindow("pano");
 //    return 25;
 
+
+
     do
     {
-        omniSys.camera_1->ReadFrame();
-        omniSys.camera_2->ReadFrame();
+//        omniSys.camera_1->ReadFrame();
+//        omniSys.camera_2->ReadFrame();
 
-        omniSys.StitchImage(1);
+        time = (double)cv::getTickCount();
 
-        cv::imshow("OmniLive",omniSys.GetPano());
+        omniSys.StitchImage();
 
-        cv::waitKey(100);
+        cv_img.header.stamp = ros::Time::now();
+
+        cv_img.image = omniSys.GetPano();
+
+        cv_img.toImageMsg(img_msg);
+
+        pub.publish(img_msg);
+
+        std::cout << "time to stitch : "<<((double)cv::getTickCount() - time) / cv::getTickFrequency()<<std::endl<<std::endl;
 
         ros::spinOnce();
 
